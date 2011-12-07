d9c9dc0e2023c05a2c199b5f8a0f1118674c8ac4
John Carr <john.carr@isotoma.com>
2011-12-07 21:33:13 +0000

    More idea sketching

--- isotoma/egglayer/plone.py
+++ isotoma/egglayer/plone.py
@@ -14,12 +14,29 @@
 
 from isotoma.egglayer.package import Package
 
+class Profile(object):
+
+    def __init__(self, profile_name='default'):
+        self.profile_name = profile_name
+        self.dependencies = []
+
 
 class PlonePackage(Package):
+
+    def __init__(self, io, name, version):
+        Package.__init__(self, io, name, version)
+        self.zcml_include_package = []
+        self.zcml_include_file = []
+        self.profiles = []
+
+    def add_profile(self, profile):
+        self.profiles.append(profile)
 
     def close(self):
         # Advertise this package to Zope
         self.entrypoint('z3c.autoinclude.plugin', 'target', 'plone')
+
+        self.add_code('configure.zcml', self.get_template('plone/configure.zcml.j2'))
 
         return Package.close(self)
 
